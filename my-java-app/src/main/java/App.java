3dc5042c7da4cab10618939fe2fafa04b39ad36e
IshaanD-RX6600 <dhimanishaan7@gmail.com>
2025-01-12 16:30:45 -0500

    Made some commits

--- my-java-app/src/main/java/App.java
+++ my-java-app/src/main/java/App.java
@@ -1,6 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        System.out.print("I like pizza\n");
-        System.out.print("I like pizza");
+        
     }
     }
---
+++ my-java-app/src/main/java/Employee.java
@@ -0,0 +1,8 @@
+public class Employee {
+    
+    public static void main(String[]args) {
+        int age = 29;
+
+        System.out.print(age);
+    }
+}
