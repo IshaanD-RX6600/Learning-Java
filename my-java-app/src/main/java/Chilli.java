632ec063eaf7bb2b1eb4a4ccc7c690515930e215
IshaanD-RX6600 <dhimanishaan7@gmail.com>
2025-01-31 11:29:51 -0500

    Made some changes to the Chilli class

--- my-java-app/src/main/java/Chilli.java
+++ my-java-app/src/main/java/Chilli.java
@@ -2,9 +2,8 @@ import java.util.Scanner;
 
 public class Chilli {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in); // Create a Scanner object to read input from the user
-
-        // Define the Scoville Heat Units (SHU) for each type of pepper
+        Scanner sc = new Scanner(System.in); 
+        
         int Poblano = 1500;
         int Mirasol = 6000;
         int Serrano = 15500;
@@ -12,18 +11,18 @@ public class Chilli {
         int Thai = 75000;
         int Habanero = 125000;
 
-        int P = sc.nextInt(); // Read the number of peppers
-        sc.nextLine(); // Consume the newline character left by nextInt()
+        int P = sc.nextInt(); 
+        sc.nextLine(); 
 
-        String[] peppers = new String[P]; // Create an array to store the names of the peppers
+        String[] peppers = new String[P];
         for (int i = 0; i < P; i++) {
-            peppers[i] = sc.nextLine(); // Read each pepper name and store it in the array
+            peppers[i] = sc.nextLine(); 
         }
-        sc.close(); // Close the Scanner object
+        sc.close(); 
 
-        int totalScovilleUnits = 0; // Initialize the total Scoville units to 0
-        for (String pepper : peppers) { // Iterate over each pepper name in the array
-            switch (pepper) { // Check the pepper name and add the corresponding SHU to the total
+        int totalScovilleUnits = 0; 
+        for (String pepper : peppers) { 
+            switch (pepper) { 
                 case "Poblano":
                     totalScovilleUnits += Poblano;
                     break;
@@ -43,11 +42,11 @@ public class Chilli {
                     totalScovilleUnits += Habanero;
                     break;
                 default:
-                    System.out.println("Unknown pepper type: " + pepper); // Print a message for unknown pepper types
+                    System.out.println("Unknown pepper type: " + pepper); 
                     break;
             }
         }
 
-        System.out.println(totalScovilleUnits); // Print the total Scoville units
+        System.out.println(totalScovilleUnits); 
     }
 }
