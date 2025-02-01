ece72d035f62fcb361d6c073758940055d504636
IshaanD-RX6600 <dhimanishaan7@gmail.com>
2025-02-01 15:48:00 -0500

    Made some more changes

--- my-java-app/src/main/java/FavouriteTimes3.java
+++ my-java-app/src/main/java/FavouriteTimes3.java
@@ -6,14 +6,52 @@ public class FavouriteTimes3 {
         scanner.close();
         int hours = 12;
         int numberOfRemainingMinutes = minutes % 60; // To find the extra minutes
-        if(minutes > 59){
+        if(minutes > 59 && minutes < 119){
             hours = 1;
-            System.out.println("The number of hours is " + hours);
-            System.out.println("The number of minutes is " + numberOfRemainingMinutes);
+            System.out.println(hours + ":" + numberOfRemainingMinutes);
+        }
+        else if(minutes > 119 && minutes < 179){
+            hours = 2;
+            System.out.println(hours + ":" + numberOfRemainingMinutes);
+        }
+        else if(minutes > 179 && minutes < 239){
+            hours = 3;
+            System.out.println(hours + ":" + numberOfRemainingMinutes);
+        }
+        else if(minutes > 239 && minutes < 299){
+            hours = 4;
+            System.out.println(hours + ":" + numberOfRemainingMinutes);
+        }
+        else if(minutes > 299 && minutes < 359){
+            hours = 5;
+            System.out.println(hours + ":" + numberOfRemainingMinutes);
+        }
+        else if(minutes > 359 && minutes < 419){
+            hours = 6;
+            System.out.println(hours + ":" + numberOfRemainingMinutes);
+        }
+        else if(minutes > 419 && minutes < 479){
+            hours = 7;
+            System.out.println(hours + ":" + numberOfRemainingMinutes);
+        }
+        else if(minutes > 479 && minutes < 539){
+            hours = 8;
+            System.out.println(hours + ":" + numberOfRemainingMinutes);
+        }
+        else if(minutes > 539 && minutes < 599){
+            hours = 9;
+            System.out.println(hours + ":" + numberOfRemainingMinutes);
+        }
+        else if(minutes > 599 && minutes < 659){
+            hours = 10;
+            System.out.println(hours + ":" + numberOfRemainingMinutes);
+        }
+        else if(minutes > 659 && minutes < 719){
+            hours = 11;
+            System.out.println(hours + ":" + numberOfRemainingMinutes);
         }
         else {
-        System.out.println("The number of hours is " + hours);
-        System.out.println("The number of minutes is " + numberOfRemainingMinutes);
+            System.out.println(hours + ":" + numberOfRemainingMinutes);
         }
     }
 }
