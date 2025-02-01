ec0efc4f59871ad092fc071814af798801a5c6c7
IshaanD-RX6600 <dhimanishaan7@gmail.com>
2025-02-01 15:42:50 -0500

    Refactor FavouriteTimes3 to correctly calculate hours and minutes based on user input

--- my-java-app/src/main/java/FavouriteTimes3.java
+++ my-java-app/src/main/java/FavouriteTimes3.java
@@ -4,10 +4,16 @@ public class FavouriteTimes3 {
         System.out.print("Enter the number of minutes: ");
         int minutes = scanner.nextInt();
         scanner.close();
-        int hours = minutes / 60;
+        int hours = 12;
         int numberOfRemainingMinutes = minutes % 60; // To find the extra minutes
+        if(minutes > 59){
+            hours = 1;
+            System.out.println("The number of hours is " + hours);
+            System.out.println("The number of minutes is " + numberOfRemainingMinutes);
+        }
+        else {
         System.out.println("The number of hours is " + hours);
-        System.out.println("The number of remaining minutes is " + numberOfRemainingMinutes);
-        
+        System.out.println("The number of minutes is " + numberOfRemainingMinutes);
+        }
     }
 }
