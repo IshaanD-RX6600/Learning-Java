77870d61e8afce711726fa1a99451e0972163951
IshaanD-RX6600 <dhimanishaan7@gmail.com>
2025-02-02 17:36:38 -0500

    Refactor input comparison logic and enhance output for mismatched strings

--- my-java-app/src/main/java/Troublesomekeys.java
+++ my-java-app/src/main/java/Troublesomekeys.java
@@ -4,15 +4,29 @@ public class Troublesomekeys {
             Scanner myScanner = new Scanner(System.in);
             String firstInputString = myScanner.nextLine();
             String secondInputString = myScanner.nextLine();
-            System.out.println(firstInputString);
-            System.out.println(secondInputString);
-            if(firstInputString != secondInputString){
-                for(int i = 0; i < firstInputString.length(); i++){
-                    if(firstInputString.charAt(i) != secondInputString.charAt(i)){
+            // System.out.println(firstInputString);
+            // System.out.println(secondInputString);
+            if(!firstInputString.equals(secondInputString))
+            {
+                for(int i = 0; i < firstInputString.length(); i++)
+                {
+                    if(firstInputString.charAt(i) != secondInputString.charAt(i))
+                    {
                         System.out.println(firstInputString.charAt(i) + " " + secondInputString.charAt(i));
                         break;
                     }
                 }
+                if(firstInputString.length() != secondInputString.length())
+                {
+                    if(firstInputString.length() == secondInputString.length() + 1)
+                    {
+                        System.out.println(firstInputString.charAt(firstInputString.length() - 1));
+                    }
+                }
         }
+                else
+                {
+                    System.out.println("No troublesome keys");
+                }   
     }
 }
