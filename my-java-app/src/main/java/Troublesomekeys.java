d47e6c11d105bd1ddb77d3cde7b757a24c8796e2
IshaanD-RX6600 <dhimanishaan7@gmail.com>
2025-02-02 19:03:02 -0500

    Im so done with this stufff brorororooror

--- my-java-app/src/main/java/Troublesomekeys.java
+++ my-java-app/src/main/java/Troublesomekeys.java
@@ -1,11 +1,80 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 public class Troublesomekeys {
          public static void main(String[] args) {
             Scanner myScanner = new Scanner(System.in);
             String firstInputString = myScanner.nextLine();
             String secondInputString = myScanner.nextLine();
-            // System.out.println(firstInputString);
-            // System.out.println(secondInputString);
+
+
+        int firstInputStringLength = firstInputString.length();
+        int secondInputStringLength = secondInputString.length();
+
+        // How maany skip keys
+        int difference = firstInputStringLength - secondInputStringLength;
+
+        String firstInputStringNoDuplicate = "";
+
+
+        if(difference > 1)
+        {
+
+            List<Character> duplicates = new ArrayList<>();
+
+            for(int i = 0; i < firstInputStringLength -1; i++)
+            {
+                for(int j = i+1; j < firstInputStringLength; j++)
+                {
+                    if (firstInputString.charAt(i) == firstInputString.charAt(j))
+                    {
+                        if (!duplicates.contains(firstInputString.charAt(i)))
+                        {
+                            duplicates.add(firstInputString.charAt(i));
+                        }
+                        break;
+                    }
+                }
+            }
+
+
+            for (Character character : duplicates) {
+                for (int i = 0; i < firstInputStringLength; i++) {
+                    if (character != firstInputString.charAt(i)) {
+                        firstInputStringNoDuplicate += firstInputString.charAt(i);
+                    }
+
+                }
+            }
+
+            //System.out.println(firstInputStringNoDuplicate);
+
+            compareStrings(firstInputStringNoDuplicate, secondInputString);
+
+            if (duplicates.size() != 0)
+            {
+                if (difference > 1)
+                {
+                    System.out.println(duplicates.get(0));
+                }
+            }
+
+        }
+        else if (difference == 0){
+            compareStrings(firstInputString, secondInputString); 
+            System.out.println("-");
+
+        }
+            
+        else{
+            compareStrings(firstInputString, secondInputString); 
+            System.out.println("-");
+        }
+        }
+
+    
+
+        private static void compareStrings(String firstInputString, String secondInputString) {
             if(!firstInputString.equals(secondInputString))
             {
                 for(int i = 0; i < firstInputString.length(); i++)
@@ -16,17 +85,7 @@ public class Troublesomekeys {
                         break;
                     }
                 }
-                if(firstInputString.length() != secondInputString.length())
-                {
-                    if(firstInputString.length() == secondInputString.length() + 1)
-                    {
-                        System.out.println(firstInputString.charAt(firstInputString.length() - 1));
-                    }
-                }
+            }
         }
-                else
-                {
-                    System.out.println("No troublesome keys");
-                }   
-    }
 }
+
