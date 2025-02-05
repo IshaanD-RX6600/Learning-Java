b46813020f3e99d45b7224bd8ed96e6983cea93a
IshaanD-RX6600 <dhimanishaan7@gmail.com>
2025-02-04 19:08:36 -0500

    Add Main class with input handling and duplicate counting logic

---
+++ my-java-app/src/main/java/HarvestWaterloo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class HarvestWaterloo {
+    public static void main(String[] args) 
+
+    {
+        Scanner myScanner = new Scanner(System.in);
+
+        String firstInputString = myScanner.nextLine();
+
+        String[][] grid = new String[6][6];
+
+        for ()
+    }
+    
+}
---
+++ my-java-app/src/main/java/Main.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        int[] inputs = new int[n];
+
+        for (int i = 0; i < n; i++)
+            {
+                inputs[i] = scanner.nextInt();
+        }
+
+            Arrays.sort(inputs); //Sorting the array
+            
+    }
+
+}
