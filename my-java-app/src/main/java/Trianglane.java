9e9cbdc62c1380efaf88f99271f008667b511743
IshaanD-RX6600 <dhimanishaan7@gmail.com>
2025-02-17 20:14:00 -0500

    Made some changes

--- my-java-app/src/main/java/Trianglane.java
+++ my-java-app/src/main/java/Trianglane.java
@@ -3,27 +3,43 @@ import java.util.Scanner;
 public class Trianglane {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number of columns: ");
         int columns = sc.nextInt();
         int rows = 2;
 
         int[][] array = new int[rows][columns];
 
-            for (int j = 0; j < columns; j++) 
-            {
-                for (int i = 0; i < rows; i++) 
-                {
-                    array[i][j] = sc.nextInt();
-                }
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                array[i][j] = sc.nextInt();
             }
-            for (int i = 0; i < rows; i++) 
-            {
-                for (int j = 0; j < columns; j++) 
-                {
-                    System.out.print(array[i][j] + " ");
-                    
-                }
-            }
+        }
         sc.close();
+
+        // System.out.println(array);
+        // Lets check the color of the triangle
+        int total = 0;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (array[i][j] == 1) {
+                    // System.out.print("black ");
+                    total = total + 1;
+                }
+            }
+            System.out.println(total * 3);
+            break;
+            // System.out.print(array[i][j] + " ");
+        }
+        System.out.println();   
+    }
+
+    void printArray(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+                
+            }
+            System.out.println();
+        }
     }
 }
